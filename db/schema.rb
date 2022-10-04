630e0a4314d980873cedb92a19f074af9e8f6db0
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-10-04 14:37:39 +0700

    DB schema for Skill migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_072525) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_04_073113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_072525) do
     t.integer "base_speed"
     t.string "element_type"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.integer "power"
+    t.integer "max_pp"
+    t.string "element_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
