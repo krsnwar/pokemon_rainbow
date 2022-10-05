651adce517ac539360933f6f1995d0955b901bee
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-10-05 09:55:52 +0700

    Add pokemon validation and default value

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,3 +1,17 @@
 class Pokemon < ApplicationRecord
+  after_initialize do
+    self.level ||= 1
+    self.current_experience ||= 0
+  end
+
   belongs_to :pokedex
+
+  validates :name, uniqueness: true, presence: true
+  validates :level, presence: true, numericality: { only_integer: true }
+  validates :max_health_point, presence: true, numericality: { only_integer: true }
+  validates :current_health_point, presence: true, numericality: { only_integer: true }
+  validates :attack, presence: true, numericality: { only_integer: true }
+  validates :defence, presence: true, numericality: { only_integer: true }
+  validates :speed, presence: true, numericality: { only_integer: true }
+  validates :current_experience, presence: true, numericality: { only_integer: true }
 end
