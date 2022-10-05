8f92bc37690fdfe8ebd2c3cbfe36bbe355075b5c
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-10-05 14:07:48 +0700

    Add pokemon route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   root 'homes#index'
+
   resources 'pokedexes' do
     get '/page/:page', action: :index, on: :collection
   end
+
   resources 'skills' do
     get '/page/:page', action: :index, on: :collection
   end
+
+  resources 'pokemons'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
