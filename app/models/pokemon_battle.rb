d8e6a2be0c863d9b7ec902e031dedab46548140b
Krisna Wardhana <krisna.wardhana@ukirama.com>
2022-10-06 15:02:51 +0700

    Add validation for pokemon

--- app/models/pokemon_battle.rb
+++ app/models/pokemon_battle.rb
@@ -13,4 +13,6 @@ class PokemonBattle < ApplicationRecord
   belongs_to :pokemon_winner, optional: true, :class_name => "Pokemon"
   belongs_to :pokemon_loser, optional: true, :class_name => "Pokemon"
   has_many :pokemons
+
+  validates :pokemon2, comparison: { other_than: :pokemon1 }
 end
